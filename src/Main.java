e1dfdb78fe19059b8bd8253cf7eabe845067ee23
Mayu Koresawa <e175770@ie.u-ryukyu.ac.jp>
2019-12-09 22:36:41 +0900

    step3 done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        try{
-            String str = null;
-            str.length();
-        }catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生したよ〜ん!");
-            System.out.println(e.getMessage());
-        }
-
+       String str = "百二十三";
+       int value =  Integer.parseInt(str);
     }
 }
